8c0bd208ac7149d3fad1566a622c8ea1adbe5160
sc0723 <89480509+sc0723@users.noreply.github.com>
2025-07-24 18:54:11 -0400

    Delete Auto-Push/Two Sum.py
    Wrong path in code

--- Auto-Push/Two Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        track = {}
-
-        for i in range(len(nums)):
-            if nums[i] in track:
-                return[i, track[nums[i]]]
-            else:
-                track[target - nums[i]] = i
