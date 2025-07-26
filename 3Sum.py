83a92aa269b2f9d6129522525c8fbdf583505b5f
Saarth Chaturvedi <saarthchaturvedi@Saarths-MacBook-Pro.local>
2025-07-26 12:06:17 -0400

    3Sum

---
+++ 3Sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if nums[i] == nums[i-1] and i > 0:
+                continue
+
+            j = i + 1
+            k = len(nums) - 1
+            target = -nums[i]
+            while j < k:
+                if nums[j] + nums[k] == target:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
+                elif nums[j] + nums[k] < target:
+                    j += 1
+                else:
+                    k -= 1
+            
+        return res
